ac323d1790376184b11f4044f537ddfec0b52576
dcep93 <dan@quizlet.com>
2020-05-05 14:46:09 -0700

    setUsername

--- firegame/src/routes/timeline/app/index.tsx
+++ firegame/src/routes/timeline/app/index.tsx
@@ -1,8 +1,28 @@
+import React from "react";
+
 import Game from "../../../shared/Game";
 
 class App extends Game {
+	inputRef: HTMLInputElement | null = null;
 	render() {
-		return `timeline function ${this.props.roomId} ${this.state.username}`;
+		return (
+			<div>
+				<p>
+					{`timeline function ${this.props.roomId} ${this.state.username}`}
+				</p>
+				<form onSubmit={this.setUsername.bind(this)}>
+					<input
+						type="text"
+						ref={(input) => (this.inputRef = input)}
+					/>
+				</form>
+			</div>
+		);
+	}
+
+	setUsername(e) {
+		e.preventDefault();
+		this.setState({ username: this.inputRef!.value });
 	}
 }
 
