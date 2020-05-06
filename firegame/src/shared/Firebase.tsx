cefe52286343599c80e84d62a19276ae94598624
dcep93 <dan@quizlet.com>
2020-05-05 17:16:14 -0700

    firebase progress

--- firegame/src/shared/Firebase.tsx
+++ firegame/src/shared/Firebase.tsx
@@ -1,24 +1,30 @@
 // https://console.firebase.google.com/u/0/project/firegame-7eb05/database/firegame-7eb05/data/~2F
 
-import app from "firebase/app";
+import firebase from "firebase/app";
+import "firebase/database";
 
 const config = { databaseURL: "https://firegame-7eb05.firebaseio.com/" };
+
+var database;
 
 class Firebase {
 	static init() {
 		console.log("init");
-		if (app.apps.length) return;
-		app.initializeApp(config);
+		firebase.initializeApp(config);
+		database = firebase.database();
 	}
 
 	static connect(path, callback) {
 		console.log("connect", path);
-		return Promise.resolve();
+		return database.ref(path).on("value", (snapshot) => {
+			var val = snapshot.val();
+			callback(val);
+		});
 	}
 
 	static set(path, obj) {
 		console.log("set", path);
-		return Promise.resolve();
+		return database.ref(path).set(obj);
 	}
 }
 
