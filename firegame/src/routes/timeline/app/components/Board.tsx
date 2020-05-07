ac20bb61540c4a2b7a8d429b93e023eaae9aad55
dcep93 <dan@quizlet.com>
2020-05-07 16:26:39 -0700

    targets

--- firegame/src/routes/timeline/app/components/Board.tsx
+++ firegame/src/routes/timeline/app/components/Board.tsx
@@ -8,15 +8,28 @@ class Board extends React.Component<{ game: GameType }> {
 		return (
 			<div className={styles.bubble}>
 				<h2>Board</h2>
-				{this.props.game.board.map(this.renderCard.bind(this))}
+				{this.getCardsAndTargets()}
 			</div>
 		);
 	}
 
-	renderCard(index: number) {
-		const term = this.props.game.terms[index];
+	getCardsAndTargets() {
+		const cardsAndTargets: JSX.Element[] = [this.renderTarget(0)];
+		this.props.game.board.forEach((termIndex: number, index: number) => {
+			cardsAndTargets.push(this.renderCard(termIndex));
+			cardsAndTargets.push(this.renderTarget(index + 1));
+		});
+		return cardsAndTargets;
+	}
+
+	renderTarget(index: number) {
+		return <p>{index}</p>;
+	}
+
+	renderCard(termIndex: number) {
+		const term = this.props.game.terms[termIndex];
 		return (
-			<div key={index} className={styles.bubble}>
+			<div key={termIndex} className={styles.bubble}>
 				<p>{term.word}</p>
 				<p>{term.definition}</p>
 				{term.image && <img src={term.image} alt="" />}
