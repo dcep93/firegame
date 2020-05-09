4324faee9cea1884de4274a28f579e35b0f7b86e
dcep93 <dan@quizlet.com>
2020-05-09 15:39:21 -0700

    server init

---
+++ firegame/server/service_script.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+screen -Dm bash -c "set -x; cd $(dirname $DIR); npm start; sh"
+
+iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 8080
+iptables -A PREROUTING -t nat -p tcp --dport 443 -j REDIRECT --to-ports 8080
---
+++ firegame/server/setup.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+APP=firegame
+
+if [ "$EUID" -ne 0 ]; then
+  echo "must run as root"
+  exit 1
+fi
+
+set -e
+set -x
+set -o pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# install git submodules
+git submodule update --init
+
+# install nodejs
+which node || ( curl -sL https://deb.nodesource.com/setup_8.x | bash - && apt-get install -y nodejs )
+
+if [ ! -d $DIR/node_modules ]; then
+	npm install
+fi
+
+# if [ ! -d $DIR/public/words ]; then
+# 	tar -zxvf $DIR/public/words.tar.gz -C $DIR/public
+# fi
+
+SERVICE_SCRIPT=$DIR/service_script.sh
+
+# server service
+cat <<END > /etc/systemd/system/$APP.service
+[Unit]
+Description=starts $APP server
+After=local-fs.target
+Wants=local-fs.target
+
+[Service]
+ExecStart=/bin/bash $SERVICE_SCRIPT
+Type=simple
+
+[Install]
+WantedBy=multi-user.target
+
+END
+systemctl daemon-reload
+systemctl enable $APP
+systemctl start $APP
