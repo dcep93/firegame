57330a0c2f47495851141e7b346762f6ec46f2aa
dcep93 <dan@quizlet.com>
2020-05-09 16:53:18 -0700

    x

--- firegame/server/app/scripts/pull.sh
+++ firegame/server/app/scripts/pull.sh
@@ -3,9 +3,9 @@
 cd "$( dirname "${BASH_SOURCE[0]}" )"
 echo "<pre>"
 echo "$(date) $(pwd) $(whoami) pull"
-git pull
+git pull 2>&1
 code1=$?
-git submodule update --init --recursive --remote
+git submodule update --init --recursive --remote  2>&1
 code2=$?
 echo "$(date) $code1 $code2 pulled"
 echo "</pre>"
