0f0d414226ed69347b57cdf92164c37bdb914401
dcep93 <dan@quizlet.com>
2020-05-09 17:51:19 -0700

    x

--- firegame/server/service_script.sh
+++ firegame/server/service_script.sh
@@ -7,5 +7,5 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 screen -Dm bash -c "set -x; pwd; cd "$DIR"; npm start; sh"
 
-iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 8080
-iptables -A PREROUTING -t nat -p tcp --dport 443 -j REDIRECT --to-ports 8080
+# iptables -A PREROUTING -t nat -p tcp --dport 80 -j REDIRECT --to-ports 8080
+# iptables -A PREROUTING -t nat -p tcp --dport 443 -j REDIRECT --to-ports 8080
