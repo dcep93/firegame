4655c3af60503877059e43170c916a0bb2b39c8a
dcep93 <dan@quizlet.com>
2020-05-09 21:25:51 -0700

    x

--- firegame/server/app/scripts/build.sh
+++ firegame/server/app/scripts/build.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 echo "<pre>"
-pwd
-cd "$( dirname "${BASH_SOURCE[0]}"/../../../ )"
-pwd
-cd /home/dcep93_2020/firegame/firegame
+cd "$( dirname "${BASH_SOURCE[0]}" )/../../../"
 pwd
 npm run build 2>&1
 code=$?
