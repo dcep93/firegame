429031e21eba70825f0f4e1c681ec0cbf8d6e3e9
dcep93 <dan@quizlet.com>
2020-05-11 13:41:10 -0700

    more structure

--- firegame/src/routes/7wd/app/main/StructureCard.tsx
+++ firegame/src/routes/7wd/app/main/StructureCard.tsx
@@ -1,5 +1,8 @@
 import React from "react";
 
+import { store } from "../utils";
+import bank from "../utils/bank";
+
 import styles from "../../../../shared/styles.module.css";
 import css from "../index.module.css";
 
@@ -16,7 +19,10 @@ class StructureCard extends React.Component<{
 			<div
 				className={`${css.structure_card} ${styles.bubble}`}
 				onClick={this.select.bind(this)}
-				style={{ transform: this.getTransform() }}
+				style={{
+					transform: this.getTransform(),
+					visibility: this.props.taken ? "hidden" : "visible",
+				}}
 			>
 				{JSON.stringify(this.props)}
 			</div>
@@ -29,7 +35,39 @@ class StructureCard extends React.Component<{
 	}
 
 	select() {
-		alert(JSON.stringify(this.props));
+		if (
+			!StructureCard.canTake(
+				this.props.y,
+				this.props.x,
+				this.props.offset
+			)
+		)
+			return alert("cannot take that card");
+		store.gameW.game.structure[this.props.y][this.props.x].taken = true;
+		const rowAboveY = this.props.y - 1;
+		const rowAbove = store.gameW.game.structure[rowAboveY];
+		if (rowAbove)
+			rowAbove.forEach(
+				(card, index) =>
+					StructureCard.canTake(rowAboveY, index, card.offset) &&
+					(card.revealed = true)
+			);
+		store.update(
+			`took ${
+				bank.bank[store.gameW.game.age]![this.props.cardIndex].name
+			}`
+		);
+	}
+
+	static canTake(y: number, x: number, offset: number): boolean {
+		const rowBelow = store.gameW.game.structure[y + 1];
+		if (!rowBelow) return true;
+		const gridX = x * 2 + offset;
+		const cardsBelow = rowBelow.filter(
+			(card, index) =>
+				!card.taken && Math.abs(card.offset + index * 2 - gridX) === 1
+		);
+		return cardsBelow.length === 0;
 	}
 }
 
