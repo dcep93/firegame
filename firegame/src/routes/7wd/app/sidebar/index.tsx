8b83345e94b930d96017bff75c4c1c7e07ad6529
dcep93 <dan@quizlet.com>
2020-05-23 12:51:04 -0700

    sidebar

--- firegame/src/routes/7wd/app/sidebar/index.tsx
+++ firegame/src/routes/7wd/app/sidebar/index.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import Players from "./Players";
 import Log from "./Log";
 
-import css from "../index.module.css";
+import styles from "../../../../shared/styles.module.css";
 
 class Sidebar extends React.Component {
 	render() {
 		return (
-			<div className={css.sidebar}>
+			<div className={styles.sidebar}>
 				<Log />
 				<Players />
 			</div>
--- firegame/src/routes/timeline/app/sidebar/index.tsx
+++ firegame/src/routes/timeline/app/sidebar/index.tsx
@@ -7,12 +7,12 @@ import Players from "./Players";
 import Info from "./Info";
 import Log from "../../../../shared/components/log";
 
-import css from "../index.module.css";
+import styles from "../../../../shared/styles.module.css";
 
 class Sidebar extends React.Component {
 	render() {
 		return (
-			<div className={css.sidebar}>
+			<div className={styles.sidebar}>
 				<Settings />
 				<Players />
 				{store.gameW.game && <Info />}
