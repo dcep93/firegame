3d15c4b40ee65913abecf9e1fa3e410c4da69eb3
dcep93 <dan@quizlet.com>
2020-06-06 13:00:33 -0700

    ll

--- firegame/src/routes/love_letter/app/LoveLetter.tsx
+++ firegame/src/routes/love_letter/app/LoveLetter.tsx
@@ -6,17 +6,18 @@ import Sidebar from "./sidebar/Sidebar";
 import Rules from "./Rules";
 
 import styles from "../../../shared/styles.module.css";
-import css from "./index.module.css";
 
 class LoveLetter extends React.Component {
 	render() {
-		const classes = [css.main];
-		if (utils.isMyTurn()) classes.push(styles.my_turn);
 		return (
 			<div>
-				<div className={classes.join(" ")}>
+				<div
+					className={`${styles.main} ${
+						utils.isMyTurn() && styles.my_turn
+					}`}
+				>
 					<Sidebar />
-					<div className={css.content}>
+					<div className={styles.content}>
 						<div>{store.gameW.game && <Main />}</div>
 						<div>
 							<Rules />
