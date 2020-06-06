80516f7e6b52a66c900efba48f6934c2e4872bd5
dcep93 <dan@quizlet.com>
2020-06-05 18:20:20 -0700

    no space in loveletter

--- firegame/src/firegame/games.tsx
+++ firegame/src/firegame/games.tsx
@@ -3,4 +3,4 @@ import timeline from "../routes/timeline/app/Timeline";
 import _7wd from "../routes/7wd/app/X7wd";
 import loveletter from "../routes/love_letter/app/LoveLetter";
 
-export default { fox, timeline, "7wd": _7wd, "Love Letter": loveletter };
+export default { fox, timeline, "7wd": _7wd, loveletter };
