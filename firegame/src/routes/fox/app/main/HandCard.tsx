c38efe271e9a80b558ed755262ac26e40f2704a1
dcep93 <dan@quizlet.com>
2020-06-07 02:36:23 -0700

    x

--- firegame/src/routes/fox/app/main/HandCard.tsx
+++ firegame/src/routes/fox/app/main/HandCard.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { store, getText } from "../utils/utils";
+import { store, getText, shared } from "../utils/utils";
 import { Card } from "../utils/NewGame";
 
 import playCard from "./playCard";
@@ -17,6 +17,7 @@ class HandCard extends React.Component<{ card: Card; index: number }> {
 	}
 
 	play() {
+		if (!shared.isMyTurn()) return;
 		const message = playCard(this.props.index);
 		if (message) store.update(message);
 	}
