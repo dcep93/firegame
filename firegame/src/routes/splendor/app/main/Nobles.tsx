68dfe48d226adecc30e7946fbfe92008a8b971be
dcep93 <dan@quizlet.com>
2020-06-14 00:40:05 -0700

    nobles

--- firegame/src/routes/splendor/app/main/Nobles.tsx
+++ firegame/src/routes/splendor/app/main/Nobles.tsx
@@ -1,10 +1,32 @@
 import React from "react";
 
 import styles from "../../../../shared/styles.module.css";
+import { store } from "../utils/utils";
+import { Token } from "../utils/bank";
 
 class Nobles extends React.Component {
 	render() {
-		return <div className={styles.bubble}>Nobles</div>;
+		const nobles = store.gameW.game.nobles;
+		if (!nobles) return null;
+		return (
+			<div className={styles.bubble}>
+				<h2>Nobles</h2>
+				{nobles.map((n) => (
+					<div>
+						{Object.entries(n)
+							.map(([token, count]) => ({
+								token: parseInt(token),
+								count,
+							}))
+							.map(
+								({ token, count }) =>
+									`${Token[token]}: ${count}`
+							)
+							.join(" ")}
+					</div>
+				))}
+			</div>
+		);
 	}
 }
 
