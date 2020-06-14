2b03486d66fdc513b1005c0c6575fbcdb57ff36d
dcep93 <dan@quizlet.com>
2020-06-14 02:02:31 -0700

    fixfox

---
+++ firegame/build/precache-manifest.702ab5903bb7d76d43bd62e586f7dfb7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a3b7ace726e0cea6de0e7a0b7ae1666",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f2bce09619d1ecb1d8ef",
+    "url": "/static/css/2.8e02db34.chunk.css"
+  },
+  {
+    "revision": "3078622f9817ad16d657",
+    "url": "/static/css/main.91cf8641.chunk.css"
+  },
+  {
+    "revision": "f2bce09619d1ecb1d8ef",
+    "url": "/static/js/2.0bfb2511.chunk.js"
+  },
+  {
+    "revision": "f42d151f61898dd9d5ffe0d9c5238b3c",
+    "url": "/static/js/2.0bfb2511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3078622f9817ad16d657",
+    "url": "/static/js/main.21062cc0.chunk.js"
+  },
+  {
+    "revision": "822371893eb75c394278",
+    "url": "/static/js/runtime-main.8dd95502.js"
+  }
+]);
