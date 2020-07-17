24df3f00955f7eacf40d70543831c03d4ccfe6fa
dcep93 <dan@quizlet.com>
2020-07-16 22:35:30 -0700

    build

--- firegame/build/precache-manifest.a582fba34321b8fc31ab012ecf288bb5.js
+++ firegame/build/precache-manifest.a582fba34321b8fc31ab012ecf288bb5.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "0a3b7ace726e0cea6de0e7a0b7ae1666",
+    "revision": "37a2d04aa4205ee9cde62c627bb78219",
     "url": "/index.html"
   },
   {
@@ -8,8 +8,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/css/2.8e02db34.chunk.css"
   },
   {
-    "revision": "3078622f9817ad16d657",
-    "url": "/static/css/main.91cf8641.chunk.css"
+    "revision": "52b61710d31df4ec025a",
+    "url": "/static/css/main.6a6ebe4e.chunk.css"
   },
   {
     "revision": "f2bce09619d1ecb1d8ef",
@@ -20,8 +20,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/static/js/2.0bfb2511.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "3078622f9817ad16d657",
-    "url": "/static/js/main.21062cc0.chunk.js"
+    "revision": "52b61710d31df4ec025a",
+    "url": "/static/js/main.e54d73c1.chunk.js"
   },
   {
     "revision": "822371893eb75c394278",
--- firegame/build/service-worker.js
+++ firegame/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.702ab5903bb7d76d43bd62e586f7dfb7.js"
+  "/precache-manifest.a582fba34321b8fc31ab012ecf288bb5.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
