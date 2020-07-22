b56fe111564544bc85c93ea1ed990c747fede3eb
dcep93 <dan@quizlet.com>
2020-07-21 21:07:44 -0700

    more ai

--- firegame/src/routes/splendor/app/utils/ai.tsx
+++ firegame/src/routes/splendor/app/utils/ai.tsx
@@ -1,24 +1,49 @@
+import { Token } from "./bank";
 import minimax from "./minimax";
-import { GameType } from "./NewGame";
-import { store } from "./utils";
+import { GameType, PlayerType } from "./NewGame";
+import utils, { store } from "./utils";
 
-function ai(depth: number, top: number = 0): void {
+function ai(depth: number, top: number): void {
+  if (store.gameW.game.players.length !== 2) {
+    console.log("need exactly 2 players");
+    return;
+  }
   const results = minimax(
     store.gameW.game,
-    state_to_children,
     heuristic,
+    state_to_children,
     depth,
     top
   );
   console.log(results.map((r) => r.join(" ")).join("\n"));
 }
 
+function heuristic(s: GameType): number {
+  const current = s.currentPlayer;
+  const me = utils.getPlayer(current);
+  const opp = utils.getPlayer(1 - current);
+  const myH = playerHeuristic(me);
+  const oppH = playerHeuristic(opp);
+  return myH - oppH;
+}
+
 function state_to_children(s: GameType): { [move: string]: GameType } {
   return {};
 }
 
-function heuristic(s: GameType): number {
-  return 0;
+function playerHeuristic(p: PlayerType): number {
+  const scoreFactor = 1;
+  const handFactor = 0.1;
+  const tokenFactor = 0.1;
+  const starFactor = 0.1;
+  const cardFactor = 0.1;
+  return (
+    scoreFactor * utils.getScore(p) +
+    handFactor * (p.hand || []).length +
+    tokenFactor * (p.tokens || []).length +
+    starFactor * (p.tokens || []).filter((t) => t === Token.gold).length +
+    cardFactor * (p.cards || []).length
+  );
 }
 
 export default ai;
--- firegame/src/routes/splendor/app/utils/minimax.tsx
+++ firegame/src/routes/splendor/app/utils/minimax.tsx
@@ -1,7 +1,7 @@
 function minimax<T>(
   state: T,
-  state_to_children: (s: T) => { [move: string]: T },
   heuristic: (s: T) => number,
+  state_to_children: (s: T) => { [move: string]: T },
   depth: number,
   top: number
 ): string[][] {
