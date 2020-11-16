3c35b83b527bb5beecc205cd089e249da65aa88a
dcep93 <dan@quizlet.com>
2020-11-16 16:30:55 -0700

    init

---
+++ firegame/src/routes/wingspan/app/main/Main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "../../../../shared/styles.module.css";
+import { store } from "../utils/utils";
+
+class Main extends React.Component {
+  render() {
+    return (
+      <div className={styles.bubble}>
+        <h2>Wingspan</h2>
+        <pre>{JSON.stringify(store.gameW.game)}</pre>
+      </div>
+    );
+  }
+}
+
+export default Main;
