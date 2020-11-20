b647f646632d1eeb6f9196219297355292bda44f
dcep93 <dan@quizlet.com>
2020-11-20 01:25:12 -0700

    todo

--- firegame/src/routes/wingspan/app/utils/NewGame.tsx
+++ firegame/src/routes/wingspan/app/utils/NewGame.tsx
@@ -72,6 +72,7 @@ function NewGame(params: Params): PromiseLike<GameType> {
 }
 
 function playWithCard(card: CardType, params: Params): boolean {
+  if (card.food_slash) return false; // todo
   if (card.food_star) return false; // todo
   switch (card.expansion) {
     case ExpansionEnum.core:
