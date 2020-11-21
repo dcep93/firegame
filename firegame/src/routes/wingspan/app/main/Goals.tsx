eacfb2c3b17996b6516c634ed2ff0b5493a39fca
dcep93 <dan@quizlet.com>
2020-11-20 19:34:08 -0700

    goals

--- firegame/src/routes/wingspan/app/main/Goals.tsx
+++ firegame/src/routes/wingspan/app/main/Goals.tsx
@@ -1,14 +1,48 @@
 import React from "react";
 import styles from "../../../../shared/styles.module.css";
+import GoalsBank from "../utils/goals_bank";
+import utils, { store } from "../utils/utils";
 
 class Goals extends React.Component {
   render() {
     return (
       <div className={styles.bubble}>
         <h2>Goals</h2>
-        <pre></pre>
+        {store.gameW.game.goals.map((gw, i) => (
+          <div>
+            {utils.goalScoring[i].map((val, j) => this.goalCell(val, i, j))}
+            <span>{GoalsBank[gw.index].goal}</span>
+          </div>
+        ))}
       </div>
     );
+  }
+
+  goalCell(val: number, i: number, j: number) {
+    const rankings = store.gameW.game.goals[i].rankings[j];
+    return (
+      <span
+        className={styles.bubble}
+        onClick={() => this.toggleGoalCell(rankings, i, j)}
+      >
+        <div>{val}</div>
+        <div>
+          {Object.entries(rankings)
+            .map(([id, selected]) => ({ id, selected }))
+            .filter((i) => i.selected)
+            .map((i) => parseInt(i.id))
+            .map((i) => `p${i + 1}`)
+            .join(" ")}
+        </div>
+      </span>
+    );
+  }
+
+  toggleGoalCell(rankings: { [n: number]: boolean }, i: number, j: number) {
+    const myIndex = utils.myIndex();
+    if (myIndex < 0) return;
+    rankings[myIndex] = !rankings[myIndex];
+    store.update(`toggled goal ${i} ${j}`);
   }
 }
 
