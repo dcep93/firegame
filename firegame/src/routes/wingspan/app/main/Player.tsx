aba902e0be40d2fdcfb8e259418188ad627b89d3
dcep93 <dan@quizlet.com>
2020-11-29 22:51:26 -0600

    undo reflip

--- firegame/src/routes/wingspan/app/main/Player.tsx
+++ firegame/src/routes/wingspan/app/main/Player.tsx
@@ -72,9 +72,9 @@ class Hand extends React.Component<{
   endTurn(): void {
     const game = store.gameW.game;
     utils.incrementPlayerTurn();
-    game.publicCards = game.publicCards.map((i) =>
-      i === -1 ? game.deck.shift()! : i
-    );
+    // game.publicCards = game.publicCards.map((i) =>
+    //   i === -1 ? game.deck.shift()! : i
+    // );
     if (utils.currentIndex() === game.startingPlayer) {
       game.turnNumber++;
       if (game.turnNumber + game.roundNumber === 10) {
