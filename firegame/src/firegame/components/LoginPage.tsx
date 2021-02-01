2450990fe6e3f7ab245842351adedb26005adb1e
dcep93 <dan@quizlet.com>
2021-01-31 17:37:41 -0700

    login focus

--- firegame/src/firegame/components/LoginPage.tsx
+++ firegame/src/firegame/components/LoginPage.tsx
@@ -1,30 +1,32 @@
 import React, { FormEvent } from "react";
-
 import store from "../../shared/store";
-import writer from "../writer/writer";
-
 import styles from "../../shared/styles.module.css";
+import writer from "../writer/writer";
 import css from "./index.module.css";
 
 class LoginPage extends React.Component {
-	inputRef: React.RefObject<HTMLInputElement> = React.createRef();
-	render() {
-		return (
-			<div className={`${css.login} ${styles.bubble}`}>
-				<p>Welcome to {store.me.gameName}!</p>
-				<p>Enter your name to continue</p>
-				<form onSubmit={this.setUsername.bind(this)}>
-					<input type="text" ref={this.inputRef} />
-				</form>
-			</div>
-		);
-	}
+  inputRef: React.RefObject<HTMLInputElement> = React.createRef();
+  render() {
+    return (
+      <div className={`${css.login} ${styles.bubble}`}>
+        <p>Welcome to {store.me.gameName}!</p>
+        <p>Enter your name to continue</p>
+        <form onSubmit={this.setUsername.bind(this)}>
+          <input type="text" ref={this.inputRef} />
+        </form>
+      </div>
+    );
+  }
 
-	setUsername(e: FormEvent<HTMLFormElement>): void {
-		e.preventDefault();
-		const username: string = this.inputRef.current!.value;
-		writer.setUsername(username);
-	}
+  componentDidMount() {
+    this.inputRef.current?.focus();
+  }
+
+  setUsername(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault();
+    const username: string = this.inputRef.current!.value;
+    writer.setUsername(username);
+  }
 }
 
 export default LoginPage;
