437aaf4c9beefe462dc2e3c687f18e1d8aadd6b5
dcep93 <dan@quizlet.com>
2021-02-10 09:06:52 -0700

    sidebar

--- firegame/src/routes/azul/app/sidebar/Sidebar.tsx
+++ firegame/src/routes/azul/app/sidebar/Sidebar.tsx
@@ -1,26 +1,24 @@
 import React from "react";
-
-import utils, { store } from "../utils/utils";
-import NewGame, { Params } from "../utils/NewGame";
-
 import SharedSidebar from "../../../../shared/components/sidebar/SharedSidebar";
+import NewGame, { Params } from "../utils/NewGame";
+import utils, { store } from "../utils/utils";
 
 class Sidebar extends SharedSidebar<Params> {
-	name = "Template";
-	NewGame = NewGame;
-	isMyTurn = utils.isMyTurn.bind(utils);
+  name = "Azul";
+  NewGame = NewGame;
+  isMyTurn = utils.isMyTurn.bind(utils);
 
-	renderStartNewGame() {
-		return (
-			<div>
-				<button onClick={this.startNewGame.bind(this)}>New Game</button>
-			</div>
-		);
-	}
+  renderStartNewGame() {
+    return (
+      <div>
+        <button onClick={this.startNewGame.bind(this)}>New Game</button>
+      </div>
+    );
+  }
 
-	getParams(): Params {
-		return { lobby: store.lobby };
-	}
+  getParams(): Params {
+    return { lobby: store.lobby };
+  }
 }
 
 export default Sidebar;
