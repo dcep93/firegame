1b798b558a068ad6d44c50dfa3604a545f30e093
dcep93 <dan@quizlet.com>
2021-02-10 18:00:18 -0700

    rules

--- firegame/src/routes/azul/app/sidebar/Sidebar.tsx
+++ firegame/src/routes/azul/app/sidebar/Sidebar.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import SharedSidebar from "../../../../shared/components/sidebar/SharedSidebar";
 import NewGame, { Params } from "../utils/NewGame";
 import utils, { store } from "../utils/utils";
@@ -6,6 +7,19 @@ class Sidebar extends SharedSidebar<Params> {
   name = "Azul";
   NewGame = NewGame;
   isMyTurn = utils.isMyTurn.bind(utils);
+
+  renderStartNewGame() {
+    return (
+      <div>
+        <button onClick={this.startNewGame.bind(this)}>New Game</button>
+        <h4>
+          <a href="https://www.ultraboardgames.com/azul/game-rules.php">
+            Rules
+          </a>
+        </h4>
+      </div>
+    );
+  }
 
   getParams(): Params {
     return { lobby: store.lobby };
