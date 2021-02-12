ddfdbecbcfb9e7faa202d84a915a73de55316358
dcep93 <dan@quizlet.com>
2021-02-12 01:29:22 -0700

    click

--- firegame/src/routes/tictactoe/app/main/Main.tsx
+++ firegame/src/routes/tictactoe/app/main/Main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../../../../shared/styles.module.css";
+import utils from "../../../7wd/app/utils/utils";
 import { store } from "../utils/utils";
 
 class Main extends React.Component {
@@ -7,17 +8,27 @@ class Main extends React.Component {
     const board = store.gameW.game.board;
     return (
       <div>
-        <div className={[styles.bubble, styles.inline].join(" ")}>
-          {board.map((row, i) => (
-            <div key={i} className={styles.flex}>
-              {row.map((cell, j) => (
-                <div key={j} className={styles.bubble}></div>
-              ))}
-            </div>
-          ))}
+        <div>
+          <div className={[styles.bubble, styles.inline].join(" ")}>
+            {board.map((row, i) => (
+              <div key={i} className={styles.flex}>
+                {row.map((cell, j) => (
+                  <div
+                    key={j}
+                    className={styles.bubble}
+                    onClick={() => this.click(i, j)}
+                  ></div>
+                ))}
+              </div>
+            ))}
+          </div>
         </div>
       </div>
     );
+  }
+
+  click(row: number, column: number) {
+    if (!utils.isMyTurn()) return;
   }
 }
 
