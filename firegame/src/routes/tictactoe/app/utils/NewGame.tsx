49166ed9926a3e5484b9208a3f33e000c67e109a
dcep93 <dan@quizlet.com>
2021-02-12 01:01:37 -0700

    new game

--- firegame/src/routes/tictactoe/app/utils/NewGame.tsx
+++ firegame/src/routes/tictactoe/app/utils/NewGame.tsx
@@ -1,38 +1,54 @@
 import { LobbyType } from "../../../../shared/store";
-
 import utils, { store } from "./utils";
 
+const SIZE = 6;
+
+export enum Tile {
+  p1,
+  p2,
+  neutral,
+  empty,
+}
+
 export type GameType = {
-	params: Params;
-	currentPlayer: number;
-	players: PlayerType[];
+  params: Params;
+  currentPlayer: number;
+  players: PlayerType[];
+  board: Tile[][];
 };
 
 export type Params = {
-	lobby: LobbyType;
+  lobby: LobbyType;
 };
 
 export type PlayerType = {
-	userId: string;
-	userName: string;
+  userId: string;
+  userName: string;
+  canPlaceNeutral: boolean;
 };
 
 function NewGame(params: Params): PromiseLike<GameType> {
-	// @ts-ignore game being constructed
-	const game: GameType = {};
-	game.params = params;
-	return Promise.resolve(game).then(setPlayers);
+  // @ts-ignore game being constructed
+  const game: GameType = {};
+  game.params = params;
+  game.board = utils.repeat(utils.repeat(Tile.empty, SIZE), SIZE);
+  return Promise.resolve(game).then(setPlayers);
 }
 
 function setPlayers(game: GameType): GameType {
-	game.players = Object.entries(store.lobby)
-		.sort((a, b) => (b[0] === store.me.userId ? 1 : -1))
-		.map(([userId, userName]) => ({
-			userId,
-			userName,
-		}));
-	game.currentPlayer = utils.myIndex(game);
-	return game;
+  game.players = Object.entries(store.lobby)
+    .sort((a, b) => (b[0] === store.me.userId ? 1 : -1))
+    .map(([userId, userName], index) => ({
+      userId,
+      userName,
+      canPlaceNeutral: index === 1,
+    }))
+    .slice(0, 2);
+  if (game.players.length !== 2) {
+    throw new Error("need 2 players");
+  }
+  game.currentPlayer = utils.myIndex(game);
+  return game;
 }
 
 export default NewGame;
