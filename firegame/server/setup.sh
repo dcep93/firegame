50751b82a6dce28df35403f8b1f85b2b81f3b256
dcep93 <dan@quizlet.com>
2021-02-26 21:06:00 -0800

    nobuild

--- firegame/server/setup.sh
+++ firegame/server/setup.sh
@@ -27,7 +27,7 @@ if [ ! -d $DIR/node_modules ]; then
 fi
 
 which yarn || apt install -y yarn
-(cd "$DIR/../" && npm install && npm run build)
+(cd "$DIR/../" && npm install)
 
 # if [ ! -d $DIR/public/words ]; then
 # 	tar -zxvf $DIR/public/words.tar.gz -C $DIR/public
