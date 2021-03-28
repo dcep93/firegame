877c1cc0daa8dd800efaa439e1030934d81f5309
dcep93 <dan@quizlet.com>
2021-03-27 23:48:00 -0700

    x

--- firegame/src/routes/spy/app/Spy.tsx
+++ firegame/src/routes/spy/app/Spy.tsx
@@ -2,16 +2,14 @@ import React from "react";
 import styles from "../../../shared/styles.module.css";
 import Main from "./main/Main";
 import Sidebar from "./sidebar/Sidebar";
-import utils, { store } from "./utils/utils";
+import { store } from "./utils/utils";
 
 class Spy extends React.Component {
   render() {
     return (
-      <div className={`${utils.isMyTurn() && styles.my_turn}`}>
-        <div className={styles.main}>
-          <Sidebar />
-          <div className={styles.content}>{store.gameW.game && <Main />}</div>
-        </div>
+      <div>
+        <div className={styles.content}>{store.gameW.game && <Main />}</div>
+        <Sidebar />
       </div>
     );
   }
