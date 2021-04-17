d591690899719370255c7ee90d481e943e5a62b5
dcep93 <dan@quizlet.com>
2021-04-16 20:08:15 -0700

    render

--- firegame/src/routes/five/app/main/Main.tsx
+++ firegame/src/routes/five/app/main/Main.tsx
@@ -1,17 +1,41 @@
 import React from "react";
+import styles from "../../../../shared/styles.module.css";
+import { Action } from "../utils/NewGame";
 import { store } from "../utils/utils";
 
-import styles from "../../../../shared/styles.module.css";
-
 class Main extends React.Component {
-	render() {
-		return (
-			<div className={styles.bubble}>
-				<h2>Main</h2>
-				<pre>{JSON.stringify(store.gameW.game)}</pre>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div>
+        <div>
+          {store.gameW.game.players.map((p, i) => (
+            <div key={i}>
+              <div className={styles.bubble}>
+                <h2>{p.userName}</h2>
+                <p>chips: {p.chips}</p>
+                {p.blocked !== undefined && <p>blocked: {Action[p.blocked]}</p>}
+                <p>
+                  lights:{" "}
+                  {Object.entries(p.lights)
+                    .filter(([a, l]) => l)
+                    .map(([a, l]) => Action[parseInt(a)])
+                    .join(" ")}
+                </p>
+                <p>
+                  last round:{" "}
+                  {(p.lastRound || []).map((a) => Action[a]).join(" ")}
+                </p>
+                {p.twoInARow !== undefined && (
+                  <p>two in a row: {Action[p.twoInARow]}</p>
+                )}
+              </div>
+            </div>
+          ))}
+        </div>
+        <div>{/* todo - actions and submit and block selection */}</div>
+      </div>
+    );
+  }
 }
 
 export default Main;
