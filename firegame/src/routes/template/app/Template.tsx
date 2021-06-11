0509c25c1d6bfd5fcd023d67326eb47013b9398e
dcep93 <dan@quizlet.com>
2021-06-11 12:51:29 -0700

    x

--- firegame/src/routes/template/app/Template.tsx
+++ firegame/src/routes/template/app/Template.tsx
@@ -1,27 +1,23 @@
 import React from "react";
-
-import utils, { store } from "./utils/utils";
+import styles from "../../../shared/styles.module.css";
 import Main from "./main/Main";
 import Sidebar from "./sidebar/Sidebar";
-
-import styles from "../../../shared/styles.module.css";
+import utils, { store } from "./utils/utils";
 
 // this folder should be as small as possible
 // common functions should be extended from shared folder!
 
 class Template extends React.Component {
-	render() {
-		return (
-			<div className={`${utils.isMyTurn() && styles.my_turn}`}>
-				<div className={styles.main}>
-					<Sidebar />
-					<div className={styles.content}>
-						{store.gameW.game && <Main />}
-					</div>
-				</div>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div
+        className={[styles.main, utils.isMyTurn() && styles.my_turn].join(" ")}
+      >
+        <Sidebar />
+        <div className={styles.content}>{store.gameW.game && <Main />}</div>
+      </div>
+    );
+  }
 }
 
 export default Template;
