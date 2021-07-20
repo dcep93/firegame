ef0501683b6a31c8bf8108e19e872826ec4308ab
dcep93 <dan@quizlet.com>
2021-07-20 15:21:53 -0700

    x

--- .github/workflows/deploy_to_app_engine.sh
+++ .github/workflows/deploy_to_app_engine.sh
@@ -6,4 +6,4 @@ cd firegame/server
 export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
 echo "$1" > "$GOOGLE_APPLICATION_CREDENTIALS"
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
-gcloud --project firegame-7eb05 app deploy
+gcloud --project firegame-320422 app deploy
