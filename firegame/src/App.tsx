385840a9d7c5491326f9056e83fbe82b6d1fa37d
dcep93 <dan@quizlet.com>
2021-07-28 22:04:21 -0700

    fmt

--- firegame/src/App.tsx
+++ firegame/src/App.tsx
@@ -1,44 +1,43 @@
 import React from "react";
 import {
-	BrowserRouter as Router,
-	Route,
-	RouteComponentProps,
+  BrowserRouter as Router,
+  Route,
+  RouteComponentProps,
 } from "react-router-dom";
-
+import GameWrapper from "./firegame/components/GameWrapper";
+import Home from "./firegame/components/Home";
 import games from "./firegame/games";
 
-import Home from "./firegame/components/Home";
-import GameWrapper from "./firegame/components/GameWrapper";
-
 function App() {
-	return (
-		<Router>
-			<div>
-				<Route exact path="/" component={Home} />
-				{getRoutes()}
-			</div>
-		</Router>
-	);
+  return (
+    <Router>
+      <div>
+        <Route exact path="/" component={Home} />
+        {getRoutes()}
+      </div>
+    </Router>
+  );
 }
 
 function getRoutes(): JSX.Element {
-	const routes: JSX.Element[] = [];
-	for (let [gameName, component] of Object.entries(games)) {
-		routes.push(
-			<Route
-				key={gameName}
-				path={`/${gameName}/:roomId(\\d+)?`}
-				render={(props: RouteComponentProps<{ roomId: string }>) => (
-					<GameWrapper
-						component={component}
-						gameName={gameName}
-						roomId={parseInt(props.match.params.roomId) || -1}
-					/>
-				)}
-			/>
-		);
-	}
-	return <>{routes}</>;
+  const routes: JSX.Element[] = [];
+  for (let [gameName, component] of Object.entries(games)) {
+    routes.push(
+      <Route
+        key={gameName}
+        path={`/${gameName}/:roomId(\\d+)?`}
+        // @ts-ignore
+        render={(props: RouteComponentProps<{ roomId: string }>) => (
+          <GameWrapper
+            component={component}
+            gameName={gameName}
+            roomId={parseInt(props.match.params.roomId) || -1}
+          />
+        )}
+      />
+    );
+  }
+  return <>{routes}</>;
 }
 
 export default App;
