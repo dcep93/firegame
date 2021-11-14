54e5c7d4cfad713b3363c24b84e02ff8dcf95472
dcep93 <dan@quizlet.com>
2021-11-13 21:03:52 -0800

    endable

--- firegame/src/routes/modern_art/app/main/ArtC.tsx
+++ firegame/src/routes/modern_art/app/main/ArtC.tsx
@@ -2,11 +2,17 @@ import React from "react";
 import styles from "../../../../shared/styles.module.css";
 import css from "../index.module.css";
 import { Art, Artist, AType } from "../utils/NewGame";
+import utils from "../utils/utils";
 
 class ArtC extends React.Component<{ a: Art; c?: string }> {
   render() {
     return (
-      <div className={styles.bubble}>
+      <div
+        className={[
+          styles.bubble,
+          utils.countArt(this.props.a.artist) === 4 && styles.endable,
+        ].join(" ")}
+      >
         <div>
           {Artist[this.props.a.artist]} - {AType[this.props.a.aType]}
         </div>
