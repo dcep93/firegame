720d612fba50e386ba9bcfdef862b059ad487093
dcep93 <dan@quizlet.com>
2022-04-19 16:27:09 -0700

    x

--- .github/workflows/deploy_to_firebase.sh
+++ .github/workflows/deploy_to_firebase.sh
@@ -7,4 +7,5 @@ export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
 echo "$1" > "$GOOGLE_APPLICATION_CREDENTIALS"
 npm install -g firebase-tools
 gcloud auth activate-service-account --key-file="$GOOGLE_APPLICATION_CREDENTIALS"
+firebase login
 firebase deploy
