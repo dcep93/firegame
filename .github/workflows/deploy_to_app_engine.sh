b05a65ffb7998fc3297b7b49a3ed3125238ea3bd
dcep93 <dan@quizlet.com>
2022-04-19 16:03:36 -0700

    x

--- .github/workflows/deploy_to_app_engine.sh
+++ .github/workflows/deploy_to_app_engine.sh
@@ -2,6 +2,15 @@
 
 set -euo pipefail
 
+# app.yaml
+# runtime: nodejs14
+
+# App Engine Deployer
+# App Engine Service Admin
+# Cloud Build Editor
+# Service Account User
+# Storage Object Admin
+
 cd firegame/server
 export GOOGLE_APPLICATION_CREDENTIALS="gac.json"
 echo "$1" > "$GOOGLE_APPLICATION_CREDENTIALS"
