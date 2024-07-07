654fba40868a16376d1b97e9ab00bb7154a1a484
dcep93 <dcep93@gmail.com>
2024-07-07 19:01:30 -0400

    x

--- app/firegame/src/routes/eclipse/app/main/ResearchView.tsx
+++ app/firegame/src/routes/eclipse/app/main/ResearchView.tsx
@@ -6,53 +6,55 @@ import utils, { store } from "../utils/utils";
 export default function ResearchView(props: { track: Track }) {
   const game = store.gameW.game;
   return (
-    <div className={styles.bubble}>
-      <h5>research:</h5>
-      <table>
-        <tbody>
-          {utils.enumArray(Track).map((t) => (
-            <tr key={t}>
-              <td>{Track[t]}</td>
-              {Object.entries(Sciences)
-                .map(([science, obj]) => ({
-                  science: science as Science,
-                  ...obj,
-                  count: game.buyableSciences.filter((s) => s === science)
-                    .length,
-                }))
-                .filter(
-                  (obj) =>
-                    t === obj.track && (t !== Track.black || obj.count > 0)
-                )
-                .map((obj) => (
-                  <td key={obj.science} title={JSON.stringify(obj)}>
-                    {obj.count === 0 ? null : (
-                      <div
-                        style={{
-                          cursor: utils.research(
-                            false,
-                            obj.science,
-                            props.track
-                          )
-                            ? "pointer"
-                            : undefined,
-                        }}
-                        onClick={() =>
-                          utils.research(true, obj.science, props.track)
-                        }
-                      >
-                        <div>{obj.science}</div>
-                        <div>
-                          ${obj.cost}/{obj.floor} (x{obj.count})
+    <div>
+      <div className={styles.bubble}>
+        <h5>research:</h5>
+        <table>
+          <tbody>
+            {utils.enumArray(Track).map((t) => (
+              <tr key={t}>
+                <td>{Track[t]}</td>
+                {Object.entries(Sciences)
+                  .map(([science, obj]) => ({
+                    science: science as Science,
+                    ...obj,
+                    count: game.buyableSciences.filter((s) => s === science)
+                      .length,
+                  }))
+                  .filter(
+                    (obj) =>
+                      t === obj.track && (t !== Track.black || obj.count > 0)
+                  )
+                  .map((obj) => (
+                    <td key={obj.science} title={JSON.stringify(obj)}>
+                      {obj.count === 0 ? null : (
+                        <div
+                          style={{
+                            cursor: utils.research(
+                              false,
+                              obj.science,
+                              props.track
+                            )
+                              ? "pointer"
+                              : undefined,
+                          }}
+                          onClick={() =>
+                            utils.research(true, obj.science, props.track)
+                          }
+                        >
+                          <div>{obj.science}</div>
+                          <div>
+                            ${obj.cost}/{obj.floor} (x{obj.count})
+                          </div>
                         </div>
-                      </div>
-                    )}
-                  </td>
-                ))}
-            </tr>
-          ))}
-        </tbody>
-      </table>
+                      )}
+                    </td>
+                  ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
--- app/firegame/src/routes/eclipse/app/main/UpgradesView.tsx
+++ app/firegame/src/routes/eclipse/app/main/UpgradesView.tsx
@@ -1,23 +1,28 @@
-import { Upgrade, Upgrades } from "../utils/library";
+import styles from "../../../../shared/styles.module.css";
+import { Diamonds, Upgrade, Upgrades } from "../utils/library";
 
 export default function UpgradesView(props: {
   updateUpgrade: (upgrade: Upgrade) => void;
 }) {
   return (
     <div>
-      {Object.entries(Upgrades)
-        .map(([upgrade, obj]) => ({
-          upgrade: upgrade as Upgrade,
-          obj,
-        }))
-        .map((obj) => (
-          <div
-            key={obj.upgrade}
-            onClick={() => props.updateUpgrade(obj.upgrade)}
-          >
-            <div>{obj.upgrade}</div>
-          </div>
-        ))}
+      <div className={styles.bubble}>
+        <h5>upgrades:</h5>
+        <div style={{ display: "flex", flexWrap: "wrap" }}></div>
+        {Object.keys(Upgrades)
+          .map((upgrade) => upgrade as Upgrade)
+          .filter((upgrade) => Diamonds[upgrade] === undefined)
+          .map((upgrade) => (
+            <div
+              key={upgrade}
+              className={styles.bubble}
+              onClick={() => props.updateUpgrade(upgrade)}
+            >
+              <div>{upgrade}</div>
+              <div>{JSON.stringify(Upgrades[upgrade])}</div>
+            </div>
+          ))}
+      </div>
     </div>
   );
 }
