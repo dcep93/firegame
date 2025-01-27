9df5f2e9a37c096aedcbd01e45f02193e900f1f7
dcep93 <dcep93@gmail.com>
2025-01-27 02:35:32 -0500

    x

--- app/firegame/src/routes/eclipse_calc/app/main/assignDamage.tsx
+++ app/firegame/src/routes/eclipse_calc/app/main/assignDamage.tsx
@@ -13,26 +13,26 @@ export function assignDamage(
   const targets = shipGroups
     .flatMap((sg) => sg || [])
     .filter((s) => s.fI !== shooterFi);
-  rolls
-    .sort((a, b) => b.roll - a.roll)
-    .sort((a, b) => b.value - a.value)
-    .map((r) =>
-      ((t) => t && (t.damage += r.value))(
-        targets
-          .filter((t) => r.roll - t.ship.values.shield >= 6)
-          // prefer order
-          .map((t) => [
-            // not overkilling
-            t.damage + r.value <= t.ship.values.hull + 1 ? 0 : 1,
-            // killing
-            t.damage + r.value >= t.ship.values.hull + 1 ? 0 : 1,
-            // shield
-            -t.ship.values.shield,
-            t,
-          ])
-          .sort()
-          .map((o) => o.reverse()[0])[0] as { damage: number } | null
-      )
+  rolls.sort((a, b) => b.roll - a.roll).sort((a, b) => b.value - a.value);
+  for (let i = 0; i < rolls.length; i++) {
+    const r = rolls[i];
+    ((t) => t && (t.damage += r.value))(
+      targets
+        .filter((t) => r.roll - t.ship.values.shield >= 6)
+        // prefer order
+        .map((t) => [
+          // not overkilling
+          t.damage + r.value <= t.ship.values.hull + 1 ? 0 : 1,
+          // killing
+          t.damage + r.value >= t.ship.values.hull + 1 ? 0 : 1,
+          // shield
+          -t.ship.values.shield,
+          [t.damage, r.value, t.ship.values.hull, rolls],
+          t,
+        ])
+        .sort()
+        .map((o) => o.slice().reverse()[0])[0] as { damage: number } | null
     );
+  }
   return shipGroups;
 }
