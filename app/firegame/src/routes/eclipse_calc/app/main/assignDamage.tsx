1e81ab6efbf6a96b2f63deb67f41d191800515c8
dcep93 <dcep93@gmail.com>
2025-01-27 01:52:29 -0500

    x

--- app/firegame/src/routes/eclipse_calc/app/main/assignDamage.tsx
+++ app/firegame/src/routes/eclipse_calc/app/main/assignDamage.tsx
@@ -16,24 +16,23 @@ export function assignDamage(
   rolls
     .sort((a, b) => b.roll - a.roll)
     .sort((a, b) => b.value - a.value)
-    .map((r) => ({
-      r,
-      t: targets
-        .filter((t) => r.roll - t.ship.values.shield >= 6)
-        // prefer order
-        .map((t) => [
-          // not overkilling
-          t.damage + r.value <= t.ship.values.hull + 1 ? 0 : 1,
-          // killing
-          t.damage + r.value >= t.ship.values.hull + 1 ? 0 : 1,
-          // shield
-          -t.ship.values.shield,
-          t,
-        ])
-        .sort()
-        .map((o) => o.reverse()[0])[0] as { damage: number },
-    }))
-    .filter(({ t }) => t)
-    .map((o) => (o.t.damage += o.r.value));
+    .map((r) =>
+      ((t) => t && (t.damage += r.value))(
+        targets
+          .filter((t) => r.roll - t.ship.values.shield >= 6)
+          // prefer order
+          .map((t) => [
+            // not overkilling
+            t.damage + r.value <= t.ship.values.hull + 1 ? 0 : 1,
+            // killing
+            t.damage + r.value >= t.ship.values.hull + 1 ? 0 : 1,
+            // shield
+            -t.ship.values.shield,
+            t,
+          ])
+          .sort()
+          .map((o) => o.reverse()[0])[0] as { damage: number } | null
+      )
+    );
   return shipGroups;
 }
