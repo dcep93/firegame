1843f34fc6a07f74848255c2b6838ba44fea2ec0
Daniel Cepeda <dcep93@gmail.com>
2025-06-16 02:04:49 -0400

    x

--- app/firegame/src/routes/eclipse_calc/app/EclipseCalc.tsx
+++ app/firegame/src/routes/eclipse_calc/app/EclipseCalc.tsx
@@ -1,20 +1,16 @@
 import React from "react";
-import styles from "../../../shared/styles.module.css";
 import Main from "./main/Main";
 import TestCases from "./main/TestCases";
 import Sidebar from "./sidebar/Sidebar";
-import utils, { store } from "./utils/utils";
+import { store } from "./utils/utils";
 
 export default class EclipseCalc extends React.Component {
   render() {
-    return (
-      <div
-        className={[styles.main, utils.isMyTurn() && styles.my_turn].join(" ")}
-      >
-        <Sidebar />
-        <div className={styles.content}>{store.gameW.game && <Main />}</div>
-      </div>
-    );
+    if (!store.gameW.game) {
+      setTimeout(() => new Sidebar({}).startNewGame());
+      return;
+    }
+    return <Main />;
   }
 }
 
--- app/firegame/src/routes/eclipse_calc/app/main/Main.tsx
+++ app/firegame/src/routes/eclipse_calc/app/main/Main.tsx
@@ -19,125 +19,132 @@ class Main extends React.Component {
     store.gameW.game.catalog = catalog;
     return (
       <div>
-        <div className={styles.flex}>
-          {store.gameW.game.fleets.map((f, fI) => (
-            <div className={styles.bubble} key={fI} style={{ height: "100%" }}>
-              <h2>
-                {fI === 0
-                  ? "defending forces of good"
-                  : "attacking forces of evil"}
-              </h2>
-              <div>
-                <select
-                  value={-1}
-                  onChange={(e) =>
-                    (({ targetValue }) =>
-                      Promise.resolve().then(() =>
-                        targetValue >= 0
-                          ? Promise.resolve()
-                              .then(() => f.push(targetValue))
-                              .then(() =>
-                                store.update(
-                                  `added ${catalog[targetValue].name}`
-                                )
-                              )
-                          : Promise.resolve()
-                              .then(() => catalog.map((ca) => ca.name))
-                              .then((existingNames) =>
-                                utils
-                                  .randomFrom(
-                                    wordList.filter(
-                                      (w) => !existingNames.includes(w)
-                                    )
-                                  )
-                                  .toUpperCase()
-                              )
-                              .then((name) =>
-                                Promise.resolve()
-                                  .then(() => f.push(catalog.length))
-                                  .then(() =>
-                                    catalog.push({
-                                      name,
-                                      values: {
-                                        count: 1,
-                                        initiative: 1,
-                                        hull: 0,
-                                        computer: 0,
-                                        shield: 0,
-                                        cannons_1: 1,
-                                        cannons_2: 0,
-                                        cannons_3: 0,
-                                        cannons_4: 0,
-                                        missiles_1: 0,
-                                        missiles_2: 0,
-                                        missiles_3: 0,
-                                        missiles_4: 0,
-                                      },
-                                    })
-                                  )
-                                  .then(() => store.update(`spawned ${name}`))
-                              )
-                      ))({
-                      targetValue: parseInt(e.target.value),
-                    })
-                  }
-                >
-                  <option value={"-1"}>add ship</option>
-                  <option value={"-2"}>new ship</option>
-                  {catalog
-                    .map((c, i) => ({ c, i }))
-                    .filter(({ i }) => !f.includes(i))
-                    .map(({ c, i }) => (
-                      <option key={i} value={i}>
-                        {c.name}
-                      </option>
-                    ))}
-                </select>
-              </div>
-              <div>
+        <div className={styles.bubble}>room: {store.me.roomId}</div>
+        <div>
+          <div className={styles.flex}>
+            {store.gameW.game.fleets.map((f, fI) => (
+              <div
+                className={styles.bubble}
+                key={fI}
+                style={{ height: "100%" }}
+              >
+                <h2>
+                  {fI === 0
+                    ? "defending forces of good"
+                    : "attacking forces of evil"}
+                </h2>
                 <div>
-                  {f
-                    .map((shipIndex, shipI) => ({
-                      ship: store.gameW.game.catalog![shipIndex],
-                      shipI,
-                    }))
-                    .filter(({ ship }) => ship)
-                    .map(({ ship, shipI }) => (
-                      <div key={shipI}>
-                        <div className={styles.bubble}>
-                          <Ship ship={ship} />
-                          <button
-                            onClick={() =>
-                              store.update(
-                                `deleted ${
-                                  catalog[f.splice(shipI, 1)[0]!].name
-                                }`
-                              )
-                            }
-                          >
-                            delete ship
-                          </button>
+                  <select
+                    value={-1}
+                    onChange={(e) =>
+                      (({ targetValue }) =>
+                        Promise.resolve().then(() =>
+                          targetValue >= 0
+                            ? Promise.resolve()
+                                .then(() => f.push(targetValue))
+                                .then(() =>
+                                  store.update(
+                                    `added ${catalog[targetValue].name}`
+                                  )
+                                )
+                            : Promise.resolve()
+                                .then(() => catalog.map((ca) => ca.name))
+                                .then((existingNames) =>
+                                  utils
+                                    .randomFrom(
+                                      wordList.filter(
+                                        (w) => !existingNames.includes(w)
+                                      )
+                                    )
+                                    .toUpperCase()
+                                )
+                                .then((name) =>
+                                  Promise.resolve()
+                                    .then(() => f.push(catalog.length))
+                                    .then(() =>
+                                      catalog.push({
+                                        name,
+                                        values: {
+                                          count: 1,
+                                          initiative: 1,
+                                          hull: 0,
+                                          computer: 0,
+                                          shield: 0,
+                                          cannons_1: 1,
+                                          cannons_2: 0,
+                                          cannons_3: 0,
+                                          cannons_4: 0,
+                                          missiles_1: 0,
+                                          missiles_2: 0,
+                                          missiles_3: 0,
+                                          missiles_4: 0,
+                                        },
+                                      })
+                                    )
+                                    .then(() => store.update(`spawned ${name}`))
+                                )
+                        ))({
+                        targetValue: parseInt(e.target.value),
+                      })
+                    }
+                  >
+                    <option value={"-1"}>add ship</option>
+                    <option value={"-2"}>new ship</option>
+                    {catalog
+                      .map((c, i) => ({ c, i }))
+                      .filter(({ i }) => !f.includes(i))
+                      .map(({ c, i }) => (
+                        <option key={i} value={i}>
+                          {c.name}
+                        </option>
+                      ))}
+                  </select>
+                </div>
+                <div>
+                  <div>
+                    {f
+                      .map((shipIndex, shipI) => ({
+                        ship: store.gameW.game.catalog![shipIndex],
+                        shipI,
+                      }))
+                      .filter(({ ship }) => ship)
+                      .map(({ ship, shipI }) => (
+                        <div key={shipI}>
+                          <div className={styles.bubble}>
+                            <Ship ship={ship} />
+                            <button
+                              onClick={() =>
+                                store.update(
+                                  `deleted ${
+                                    catalog[f.splice(shipI, 1)[0]!].name
+                                  }`
+                                )
+                              }
+                            >
+                              delete ship
+                            </button>
+                          </div>
                         </div>
-                      </div>
-                    ))}
+                      ))}
+                  </div>
                 </div>
               </div>
+            ))}
+            <div className={styles.bubble} style={{ height: "100%" }}>
+              <button
+                onClick={() =>
+                  Promise.resolve()
+                    .then(() => store.gameW.game.fleets.reverse())
+                    .then(() => store.update("swapped fleets"))
+                }
+              >
+                swap fleets
+              </button>
             </div>
-          ))}
-          <div className={styles.bubble} style={{ height: "100%" }}>
-            <button
-              onClick={() =>
-                Promise.resolve()
-                  .then(() => store.gameW.game.fleets.reverse())
-                  .then(() => store.update("swapped fleets"))
-              }
-            >
-              swap fleets
-            </button>
           </div>
-        </div>
-        <div>
-          <Outcomes />
+          <div>
+            <Outcomes />
+          </div>
         </div>
       </div>
     );
