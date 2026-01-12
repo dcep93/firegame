c94b5d70189a4b058db6274383d6f80100dbb3d1
Daniel Cepeda <dcep93@gmail.com>
2026-01-12 01:34:56 -0500

    x

--- app/firegame/src/routes/eclipse_calc/app/EclipseCalc.tsx
+++ app/firegame/src/routes/eclipse_calc/app/EclipseCalc.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Main from "./main/Main";
-import TestCases from "./main/TestCases";
 
 export default class EclipseCalc extends React.Component {
   render() {
     return <Main />;
   }
 }
-
-console.log({ testCaseFailures: TestCases() });
