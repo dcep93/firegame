a2ddee638dc463640476c6a423e26415fd97dc58
Daniel Cepeda <dcep93@gmail.com>
2026-01-17 05:21:50 -0500

    x

--- app/firegame/src/routes/catan/app/main/Main.tsx
+++ app/firegame/src/routes/catan/app/main/Main.tsx
@@ -1,3 +1,12 @@
 export default function Main() {
-  return <div>main</div>;
+  const src = `${window.location.origin}/public_catann`;
+  return (
+    <div>
+      <iframe
+        title={src}
+        src={src}
+        style={{ width: "100vW", height: "100vH" }}
+      />
+    </div>
+  );
 }
