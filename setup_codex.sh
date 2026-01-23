400cb345b8541dfe2b74d58c34bf6576a2ae5e7f
Daniel Cepeda <dcep93@gmail.com>
2026-01-22 21:35:15 -0500

    x

--- setup_codex.sh
+++ setup_codex.sh
@@ -3,6 +3,10 @@ set -xe
 
 date
 git rev-parse HEAD
+CODEX_HOME="${CODEX_HOME:-$HOME/.codex}"
+echo $CODEX_HOME
+SKILLS_DIR="${CODEX_HOME}/skills"
+ln -sfn "$(pwd)/.codex/skills" "${SKILLS_DIR}"
 cd app/firegame
 cat package.json
 yarn install
