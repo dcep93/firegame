39ec1308df66872e66dfaa841a92c2db0f0c0c1a
Daniel Cepeda <dcep93@gmail.com>
2026-01-22 22:18:58 -0500

    x

--- setup_codex.sh
+++ setup_codex.sh
@@ -3,10 +3,10 @@ set -xe
 
 date
 ln -sfn "$(pwd)/.codex/skills" "/opt/codex/skills"
-# git rev-parse HEAD
-# cd app/firegame
-# cat package.json
-# yarn install
-# npx -y playwright install
-# npx -y playwright install-deps
-# date
+git rev-parse HEAD
+cd app/firegame
+cat package.json
+yarn install
+npx -y playwright install
+npx -y playwright install-deps
+date
