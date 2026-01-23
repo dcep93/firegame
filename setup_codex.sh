b501a527e98d777c4d51e992759b749bd5aa9d5d
Daniel Cepeda <dcep93@gmail.com>
2026-01-22 22:14:04 -0500

    x

--- setup_codex.sh
+++ setup_codex.sh
@@ -2,15 +2,14 @@
 set -xe
 
 date
-git rev-parse HEAD
-CODEX_HOME="${CODEX_HOME:-$HOME/.codex}"
 echo $CODEX_HOME
 SKILLS_DIR="${CODEX_HOME}/skills"
 mkdir -p "${CODEX_HOME}"
 ln -sfn "$(pwd)/.codex/skills" "${SKILLS_DIR}"
-cd app/firegame
-cat package.json
-yarn install
-npx -y playwright install
-npx -y playwright install-deps
-date
+# git rev-parse HEAD
+# cd app/firegame
+# cat package.json
+# yarn install
+# npx -y playwright install
+# npx -y playwright install-deps
+# date
