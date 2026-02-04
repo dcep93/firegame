64b2350836bb46d64115c071263447b327f250fe
Daniel Cepeda <dcep93@gmail.com>
2026-02-03 21:32:35 -0500

    x

--- app/firegame/src/routes/catann/test/autoChoreo.ts
+++ app/firegame/src/routes/catann/test/autoChoreo.ts
@@ -7,31 +7,37 @@ export default async function autoChoreo(
   c: ControllerType,
   stopClientDataSequence: number = -1,
 ) {
-  await c.verifyTestMessages();
-  const msg = c._peek();
-  if (!msg) return; // fastForward
-  try {
-    expect(msg.trigger).toBe("clientData");
-  } catch (e) {
-    console.log(JSON.stringify(msg, null, 2));
-    throw e;
+  for (let i = 0; true; i++) {
+    await c.verifyTestMessages();
+    const msg = c._peek();
+    if (!msg) return; // fastForward
+    try {
+      expect(msg.trigger).toBe("clientData");
+    } catch (e) {
+      console.log(JSON.stringify(msg, null, 2));
+      throw e;
+    }
+    console.log(
+      "autoChoreo",
+      msg.data.sequence,
+      i,
+      GAME_ACTION[msg.data.action],
+    );
+    if (msg.data.sequence === stopClientDataSequence) return;
+    if (msg.data.action === GAME_ACTION.PassedTurn) {
+      await c.passTurn();
+    } else if (msg.data.action === GAME_ACTION.ClickedDice) {
+      await c.rollNextDice();
+    } else if (msg.data.action === GAME_ACTION.WantToBuildRoad) {
+      await c.wantToBuildRoad();
+    } else if (msg.data.action === GAME_ACTION.WantToBuildSettlement) {
+      await c.wantToBuildSettlement();
+    } else if (msg.data.action === GAME_ACTION.BuyDevelopmentCard) {
+      await c.buyDevelopmentCard();
+    } else if (msg.data.action === GAME_ACTION.SelectedTile) {
+      await c.playNextRobber();
+    } else {
+      return;
+    }
   }
-  console.log("autoChoreo", msg.data.sequence, GAME_ACTION[msg.data.action]);
-  if (msg.data.sequence === stopClientDataSequence) return;
-  if (msg.data.action === GAME_ACTION.PassedTurn) {
-    await c.passTurn();
-  } else if (msg.data.action === GAME_ACTION.ClickedDice) {
-    await c.rollNextDice();
-  } else if (msg.data.action === GAME_ACTION.WantToBuildRoad) {
-    await c.wantToBuildRoad();
-  } else if (msg.data.action === GAME_ACTION.WantToBuildSettlement) {
-    await c.wantToBuildSettlement();
-  } else if (msg.data.action === GAME_ACTION.BuyDevelopmentCard) {
-    await c.buyDevelopmentCard();
-  } else if (msg.data.action === GAME_ACTION.SelectedTile) {
-    await c.playNextRobber();
-  } else {
-    return;
-  }
-  await autoChoreo(c, stopClientDataSequence);
 }
