1b4a018e34d9052ff93be233c149141d73bb9e54
Daniel Cepeda <dcep93@gmail.com>
2026-05-16 18:27:22 -0400

    Move colonist ship to goods supply

--- app/firegame/src/routes/puerto_rico/app/main/BankView.tsx
+++ app/firegame/src/routes/puerto_rico/app/main/BankView.tsx
@@ -10,8 +10,6 @@ function BankView() {
       <h3 className={css.heading}>Board</h3>
       <div className={css.boardGrid}>
         <div className={`${css.tile} ${css.boardTile}`}>
-          <strong className={css.tileTitle}>Colonist ship {bank.colonistShip}/{bank.colonistSupply}</strong>
-          <div className={css.cardSeparator} />
           <strong className={css.tileTitle}>Plantations</strong>
           <div className={css.boardStatsLine}>
             <span>Deck {bank.plantationDeck.length}</span>
@@ -65,6 +63,8 @@ function BankView() {
           </div>
         </div>
         <div className={`${css.tile} ${css.boardTile}`}>
+          <strong className={css.tileTitle}>Colonist ship {bank.colonistShip}/{bank.colonistSupply}</strong>
+          <div className={css.cardSeparator} />
           <strong className={css.tileTitle}>Goods supply</strong>
           {goodsInThemeOrder.map((good) => (
             <div
