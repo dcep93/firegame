e8c6e527be425766fb6e3100ad48c196d5cfe143
Daniel Cepeda <dcep93@gmail.com>
2026-05-18 01:41:58 -0400

    x

--- app/firegame/src/routes/puerto_rico/app/main/RoleRow.tsx
+++ app/firegame/src/routes/puerto_rico/app/main/RoleRow.tsx
@@ -1,10 +1,16 @@
 import css from "../index.module.css";
 import { theme } from "../theme/base";
 import { GameType } from "../utils/NewGame";
+import { GoodId, ROLE_KIND } from "../utils/rules";
 import utils, { store } from "../utils/utils";
 
 function RoleRow(props: { game?: GameType; readOnly?: boolean }) {
   const game = props.game || store.gameW.game;
+  const canChooseCraftsmanBonus =
+    !props.readOnly && game.phase === "craftsman_bonus" && utils.isMyTurn();
+  const craftsmanBonusGoods = canChooseCraftsmanBonus
+    ? game.producedGoods?.[game.roleOwner || 0] || []
+    : [];
   return (
     <div className={css.section}>
       <h3 className={css.heading}>{theme.labels.roles}</h3>
@@ -22,6 +28,20 @@ function RoleRow(props: { game?: GameType; readOnly?: boolean }) {
                 <span className={css.roleTakenChip}>{game.players[role.takenBy]?.userName}</span>
               )}
             </div>
+            {ROLE_KIND[role.id] === "craftsman" && canChooseCraftsmanBonus && (
+              <div className={css.roleActionRow}>
+                {craftsmanBonusGoods.map((good) => (
+                  <CraftsmanBonusButton key={good} good={good} />
+                ))}
+                <button
+                  type="button"
+                  className={css.inlineActionButton}
+                  onClick={() => utils.skipCraftsmanBonus()}
+                >
+                  {theme.controls.skipBonus}
+                </button>
+              </div>
+            )}
             </>
           );
           return canChoose ? (
@@ -40,6 +60,19 @@ function RoleRow(props: { game?: GameType; readOnly?: boolean }) {
         })}
       </div>
     </div>
+  );
+}
+
+function CraftsmanBonusButton(props: { good: GoodId }) {
+  return (
+    <button
+      type="button"
+      className={css.inlineActionButton}
+      disabled={store.gameW.game.bank.goodsSupply[props.good] <= 0}
+      onClick={() => utils.chooseCraftsmanBonus(props.good)}
+    >
+      {theme.actions.take} {theme.goods[props.good]}
+    </button>
   );
 }
 
